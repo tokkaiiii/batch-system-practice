ced6cd6751831f180c3a47937fe6599f668a0d6e
heosangbeom <luv_3_u@naver.com>
2025-05-26 09:02:02 +0900

    first commit

---
+++ src/main/java/com/test/batchsystempractice/BatchSystemPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.test.batchsystempractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSystemPracticeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BatchSystemPracticeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/test/batchsystempractice/BatchSystemPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.batchsystempractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSystemPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
