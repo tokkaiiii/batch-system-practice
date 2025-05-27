97e8cea1760220e2479edf89ab10be377e33636d
heosangbeom <luv_3_u@naver.com>
2025-05-27 16:12:52 +0900

    feat: 파일 작성 기능 추가

--- src/main/java/com/test/batchsystempractice/BatchFileTestConfig.java
+++ src/main/java/com/test/batchsystempractice/BatchFileTestConfig.java
@@ -3,6 +3,7 @@ package com.test.batchsystempractice;
 import java.beans.PropertyEditor;
 import java.beans.PropertyEditorSupport;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Map;
 import lombok.Data;
@@ -14,9 +15,12 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -45,9 +49,10 @@ public class BatchFileTestConfig {
       JobRepository jobRepository
   ) {
     return new StepBuilder("fileTestStep", jobRepository)
-        .<TestFile, TestFile>chunk(10, transactionManager)
+        .<TestFile, TestOutputFile>chunk(10, transactionManager)
         .reader(fileReader(null))
-        .writer(items -> items.forEach(System.out::println))
+        .processor(itemProcessor())
+        .writer(itemWriter(null))
         .build();
   }
 
@@ -78,9 +83,44 @@ public class BatchFileTestConfig {
     };
   }
 
-  /*@Bean
-  public ItemWriter<TestFile> itemWriter(){
-  }*/
+  @Bean
+  @StepScope
+  public FlatFileItemWriter<TestOutputFile> itemWriter(
+      @Value("#{jobParameters['outputDir']}") String outputDir
+  ){
+    return new FlatFileItemWriterBuilder<TestOutputFile>()
+        .name("itemWriter")
+        .resource(new FileSystemResource(outputDir + "/test_output.csv"))
+        .delimited()
+        .delimiter(",")
+        .names("id", "bookName", "author", "date", "recordDate")
+        .headerCallback(writer -> writer.write("ID,책이름,저자,집필 날짜,작성일"))
+        .build();
+  }
+
+  @Bean
+  public ItemProcessor<TestFile, TestOutputFile> itemProcessor(){
+    return item -> {
+      TestOutputFile outputFile = new TestOutputFile();
+      // 레코드 생성 날짜를 현재 날짜로 설정
+      outputFile.setId(item.getId());
+      outputFile.setBookName(item.getBookName());
+      outputFile.setAuthor(item.getAuthor());
+      outputFile.setDate(item.getDate().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")));
+      outputFile.setRecordDate(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH시 mm분 ss초")));
+      return outputFile;
+    };
+  }
+
+  @Data
+  public static class TestOutputFile {
+
+    private String id;
+    private String bookName;
+    private String author;
+    private String date; // 날짜는 문자열로 처리
+    private String recordDate; // 레코드 생성 날짜
+  }
 
   @Data
   public static class TestFile {
